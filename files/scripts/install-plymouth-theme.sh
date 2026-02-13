643d694cf4698996803e4a446906ec6e8db0fc50
PolyCat <contact.polycat@proton.me>
2026-02-13 08:55:45 +0200

    Removed nvidia_peermem and nvidia_uvm from initramfs

--- files/scripts/install-plymouth-theme.sh
+++ files/scripts/install-plymouth-theme.sh
@@ -9,3 +9,5 @@ cp -r plymouth-themes/pack_1/connect /usr/share/plymouth/themes/
 rm -rf plymouth-themes
 
 plymouth-set-default-theme connect
+
+echo 'omit_drivers+=" nvidia_peermem nvidia_uvm "' > /etc/dracut.conf.d/omit-uneeded-nvidia-modules.conf
