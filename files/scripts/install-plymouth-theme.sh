7ce40ab50c83a44db5f790f0e26d3ecec080c578
PolyCat <contact.polycat@proton.me>
2026-02-13 10:03:16 +0200

    Revert "Removed nvidia_peermem and nvidia_uvm from initramfs"
    This reverts commit 643d694cf4698996803e4a446906ec6e8db0fc50.

--- files/scripts/install-plymouth-theme.sh
+++ files/scripts/install-plymouth-theme.sh
@@ -9,5 +9,3 @@ cp -r plymouth-themes/pack_1/connect /usr/share/plymouth/themes/
 rm -rf plymouth-themes
 
 plymouth-set-default-theme connect
-
-echo 'omit_drivers+=" nvidia_peermem nvidia_uvm "' > /etc/dracut.conf.d/omit-uneeded-nvidia-modules.conf
